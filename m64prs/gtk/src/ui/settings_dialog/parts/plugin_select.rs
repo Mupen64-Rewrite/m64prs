d7a5c6c874920e7625d5c75b854416b9558fd466
jgcodes2020 <j349guo@uwaterloo.ca>
2025-01-06 20:33:18 -0500

    expand plugin selector into a full widget of sorts

--- m64prs/gtk/src/ui/settings_dialog/parts/plugin_select.rs
+++ m64prs/gtk/src/ui/settings_dialog/parts/plugin_select.rs
@@ -1,14 +1,25 @@
 mod inner {
-    use std::cell::RefCell;
+    use std::{
+        cell::{Cell, RefCell},
+        sync::{LazyLock, OnceLock},
+    };
 
+    use glib::subclass::Signal;
     use gtk::{prelude::*, subclass::prelude::*};
 
     #[derive(gtk::CompositeTemplate, glib::Properties)]
     #[template(file = "plugin_select.ui")]
     #[properties(wrapper_type = super::PluginSelect)]
     pub struct PluginSelect {
-        #[property(get)]
-        plugins: gio::ListStore,
+        #[property(get, set = |this: &PluginSelect, value: &gio::ListModel| {
+            assert!(value.item_type().is_a(gio::File::static_type()));
+            this.plugins.replace(value.clone());
+        }, type = gio::ListModel)]
+        plugins: RefCell<gio::ListModel>,
+        #[property(get, set)]
+        settings_available: Cell<bool>,
+        #[property(get, set)]
+        current_index: Cell<u32>,
     }
 
     #[gtk::template_callbacks]
@@ -20,6 +31,11 @@ mod inner {
                 .and_then(|path| path.file_name())
                 .map_or("".to_string(), |name| name.to_string_lossy().to_string())
         }
+
+        #[template_callback]
+        fn open_settings_btn(&self, btn: &gtk::Button) {
+            self.obj().emit(<Self as ObjectImpl>::signals()[0].signal_id(), &[])
+        }
     }
 
     #[glib::object_subclass]
@@ -30,13 +46,19 @@ mod inner {
         type ParentType = gtk::Frame;
 
         fn new() -> Self {
+            let plugins = gio::ListStore::new::<gio::File>();
+            plugins.append(&gio::File::for_path("/usr/lib/mupen64plus/mupen64plus-video-rice.so"));
+            plugins.append(&gio::File::for_path("/usr/lib/mupen64plus/mupen64plus-video-glide64mk2.so"));
             Self {
-                plugins: gio::ListStore::new::<gio::File>(),
+                plugins: RefCell::new(plugins.upcast()),
+                settings_available: Cell::new(false),
+                current_index: Cell::new(gtk::INVALID_LIST_POSITION),
             }
         }
 
         fn class_init(class: &mut Self::Class) {
             class.bind_template();
+            class.bind_template_callbacks();
         }
 
         fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
@@ -44,7 +66,14 @@ mod inner {
         }
     }
 
-    impl ObjectImpl for PluginSelect {}
+    #[glib::derived_properties]
+    impl ObjectImpl for PluginSelect {
+        fn signals() -> &'static [Signal] {
+            static SIGNALS: LazyLock<[Signal; 1]> =
+                LazyLock::new(|| [Signal::builder("open-settings").build()]);
+            &*SIGNALS
+        }
+    }
     impl WidgetImpl for PluginSelect {}
     impl FrameImpl for PluginSelect {}
 }
